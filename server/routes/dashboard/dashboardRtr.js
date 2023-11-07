c9ab1597718f62e2420faee2af8308253000187f
Rajkumaaryantrapati <rajkumar@greenlanternit.com>
2023-11-07 13:04:44 +0530

    Device_api changes

--- server/routes/dashboard/dashboardRtr.js
+++ server/routes/dashboard/dashboardRtr.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var dashboardRtr = express.Router();
 var appRoot='/var/www/html/glits/code/nodejs/NMS_server'
+
 const dashboardCtrl=require(appRoot + '/server/modules/Controllers/dashboard/dashboardCtrl')
 
 
