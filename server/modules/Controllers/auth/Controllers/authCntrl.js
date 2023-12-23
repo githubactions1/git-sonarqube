ad356e8382fe27a83e2af85ab850f5bd32cfd098
Rajkumaaryantrapati <rajkumar@greenlanternit.com>
2023-12-24 00:06:01 +0530

    ch

--- server/modules/Controllers/auth/Controllers/authCntrl.js
+++ server/modules/Controllers/auth/Controllers/authCntrl.js
@@ -2,6 +2,7 @@ var appRoot ='/home/nms-backend/actions-runner/_work/nms_node/nms_node';
 var authMdl = require('../Models/authMdl');
 var df = require( '../../../../../utils/dflower.utils');
 var cntxtDtls = df.getModuleMetaData(__dirname, __filename);
+var jsonwebtoken=require('jsonwebtoken')
 
 
 
@@ -12,38 +13,73 @@ var cntxtDtls = df.getModuleMetaData(__dirname, __filename);
  * 02-11-2023 - RajKumar 
  * 
  *******************************************************************************************************/
-exports.loginCntrl = function (req, res) {
-    fnm = "login";
-    // Check if 'req.body' exists and 'req.body.data' exists within it
-    if (req.body ) {
-        var req_body = req.body ? req.body: req.body.data;
-        console.log(req_body); // Log the request body to check its structure
-        authMdl.loginMdl(req_body)
-            .then(function (usrDtls) {
-                console.log('errrrrrrrrrrrrrrrrrrrrrrrrrrrrr1111222222222');                    
-                console.log(usrDtls.length,'eeeeeeeeeeeeeeeeeeeeeeeee');       
-                if (usrDtls && usrDtls.length === 0) {
-                    let error = 'Please check your Credentials';
-                  return df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {error_status: 400,err_message : "Please check your Credentials"});
-                }
-                if (!usrDtls || usrDtls === undefined){
-                let error = 'Please check your Credentials';
-                return df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {error_status: 400,err_message : "Please check your credentials"}); 
-                }
-                 else {
+// exports.loginCntrl = function (req, res) {
+//     fnm = "login";
+//     // Check if 'req.body' exists and 'req.body.data' exists within it
+//     if (req.body ) {
+//         var req_body = req.body ? req.body: req.body.data;
+//         console.log(req_body); // Log the request body to check its structure
+//         authMdl.loginMdl(req_body)
+//             .then(function (usrDtls) {
+//                 console.log('errrrrrrrrrrrrrrrrrrrrrrrrrrrrr1111222222222');                    
+//                 console.log(usrDtls.length,'eeeeeeeeeeeeeeeeeeeeeeeee');       
+//                 if (usrDtls && usrDtls.length === 0) {
+//                     let error = 'Please check your Credentials';
+//                   return df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {error_status: 400,err_message : "Please check your Credentials"});
+//                 }
+//                 if (!usrDtls || usrDtls === undefined){
+//                 let error = 'Please check your Credentials';
+//                 return df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {error_status: 400,err_message : "Please check your credentials"}); 
+//                 }
+//                  else {
     
-                    return df.formatSucessRes(req, res, usrDtls, cntxtDtls, fnm, {});
-                }              
+//                     return df.formatSucessRes(req, res, usrDtls, cntxtDtls, fnm, {});
+//                 }              
                             
+//             });
+//     } else {
+//         // Handle the case where 'req.body' or 'req.body.data' is not defined
+//         let error = 'Request body or data is missing';
+// 		console.log()
+//         df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {});
+//     }
+// }
+exports.loginCntrl = function(req, res){
+    fnm = "loginCntrl"
+    var privateKey = 'GVMCwaterDev101222';
+    req_body = req.body ? req.body : req.body.data;
+    authMdl.loginMdl(req_body).then(function (usrDtls) {
+        if (usrDtls && usrDtls.length < 0) {
+			let error = 'no data found'
+             return df.formatErrorRes(req, res, error, cntxtDtls, fnm, {});
+        } else {
+            return usrDtls;
+        }
+    }).then((usrDtls) => {
+        if (usrDtls != undefined && usrDtls.length > 0) {
+            var data = {};
+            //console.log(usrDtls)
+            let payload;
+			//var accessToken = jwt.sign(payload, privateKey, { expiresIn: '3h' , algorithm: 'HS256'} ); //{ algorithm: 'HS256'});
+            payload = Object.assign({}, usrDtls[0], {
+                app: req_body.app,
+                cmpnt_id: req_body.cmpnt_id,
+                //prt_in: usrDtls.login_details.prt_in,
+                //caf_in: usrDtls.login_details.caf_in,
+                //user_id: usrDtls.login_details.mrcht_usr_id,
             });
-    } else {
-        // Handle the case where 'req.body' or 'req.body.data' is not defined
-        let error = 'Request body or data is missing';
-		console.log()
-        df.formatloginErrorRes(req, res, error, cntxtDtls, fnm, {});
-    }
+            data.user = payload;
+            var accessToken = jwt.sign(payload, privateKey, { expiresIn: '3h' , algorithm: 'HS256'} ); //{ algorithm: 'HS256'});
+            req.user = payload;
+			data.token = accessToken;
+            res.setHeader('x-access-token', accessToken);
+            //operations.record('lgn_ct');
+            df.formatSucessRes(req, res, data, cntxtDtls, fnm, {});
+        } else {
+            df.formatErrorRes(req, res, false, cntxtDtls, fnm, {});
+        }
+    })
 }
-
 
 /******************************************************************************************************
  * Controller : register
