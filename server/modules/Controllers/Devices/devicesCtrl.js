e919fecab3a13109d163a60b4079a6f99bdefeab
Rajkumar <rajkumar@greenlanternit.com>
2024-01-05 10:41:17 +0530

    cha

--- server/modules/Controllers/Devices/devicesCtrl.js
+++ server/modules/Controllers/Devices/devicesCtrl.js
@@ -692,12 +692,12 @@ exports.updatepasswordCtrl=(req,res)=>{
 	   const old_password=result[0].user_password
 	   console.log(old_password,'old_password')
 	   const  new_password=req.body.password
-	   const sha1=sha1(new_password)
+	   const decrypt=sha1(new_password)
 	   console.log(new_password,'new_password')
-	   console.log(sha1,'sah1111111111111')
+	   console.log(decrypt,'sah1111111111111')
 	   console.log(result.length,'')
 	   if(result.length==1){
-			if(old_password==sha1){
+			if(old_password==decrypt){
 			const messag='Old Password And New Password Must Be Different '
 				df.formatErrorRes(req,res,messag,cntxtDtls,'',{});
 			}
