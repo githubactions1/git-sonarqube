c4b3c11614df3534bcf7aee00c07f080ca025f50
Rajkumar <rajkumar@greenlanternit.com>
2024-01-08 11:43:48 +0530

    changes

--- server/modules/Controllers/auth/Controllers/authCntrl.js
+++ server/modules/Controllers/auth/Controllers/authCntrl.js
@@ -100,6 +100,7 @@ exports.loginCntrl = function(req, res){
 exports.registerCntrl = function(req,res){ 
     var fnm = "registerCntrl"
     var req_body = req.body ? req.body: req.body.data; 
+    console.log(req_body,'logggggggggggggggggggggggggggggggg')
     const string= req_body.user_email.toString()
     console.log(string)
     const emailstring=string.endsWith("@gmail.com")
